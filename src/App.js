6cb1e649efe231d52f3f2933a2c7f0f6299d00d8
Elena Perers <elena.perers@chasacademy.se>
2019-09-06 16:56:01 +0200

    Add header and footer in App.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,31 @@ import Report from './Report.js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <nav>
-            <ul>
-              <li><Link to="/">Me</Link></li>
-              <li><Link to="/reports/week/1">kmom01</Link></li>
-            </ul>
-          </nav>
-          <Route exact path="/" component={Me} />
-          <Route path="/reports/week/:week" component={Report} />
-          <footer></footer>
-        </div>
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <header>
+                        <h1>JSRamverk</h1>
+                        <nav>
+                            <ul>
+                                <li><Link to="/">Me</Link></li>
+                                <li><Link to="/reports/week/1">Kmom01</Link></li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <Route exact path="/" component={Me} />
+                    <Route path="/reports/week/:week" component={Report} />
+                    <footer>
+                        <p className="social"> <a href="https://github.com/pererselena"><i class="fab fa-github fa-3x"></i></a> <a href="https://www.linkedin.com/in/elena-perers/"><i class="fab fa-linkedin-in fa-3x"></i></a></p>
+                        <p className="copyright">Copyright &copy; Elena Perers 2019</p>
+                    </footer>
+                </div>
 
-      </Router>
+            </Router>
 
-    );
-  }
+        );
+    }
 }
 
 export default App;
