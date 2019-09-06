71c129d3ccf7558feb96a7979b6d2c7809751fe1
Elena Perers <elena.perers@chasacademy.se>
2019-09-06 17:32:16 +0200

    Created component and img maps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
-import Me from './Me.js';
-import Report from './Report.js';
+import Me from './components/Me.js';
+import Report from './components/Report.js';
 
 import './App.css';
 
@@ -23,7 +23,7 @@ class App extends Component {
                     <Route exact path="/" component={Me} />
                     <Route path="/reports/week/:week" component={Report} />
                     <footer>
-                        <p className="social"> <a href="https://github.com/pererselena"><i class="fab fa-github fa-3x"></i></a> <a href="https://www.linkedin.com/in/elena-perers/"><i class="fab fa-linkedin-in fa-3x"></i></a></p>
+                        <p className="social"> <a href="https://github.com/pererselena"><i className="fab fa-github fa-3x"></i></a> <a href="https://www.linkedin.com/in/elena-perers/"><i className="fab fa-linkedin-in fa-3x"></i></a></p>
                         <p className="copyright">Copyright &copy; Elena Perers 2019</p>
                     </footer>
                 </div>
