cbcc9699baf15b8c0562dbf2ec712765efd0e0f7
Elena Perers <elena.perers@chasacademy.se>
2019-09-22 17:11:04 +0200

    Added functionality to fetch code from api.

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,50 +1,30 @@
-import React, {
-    Component
-} from 'react';
-import profileImg from '../img/me.jpg';
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 
 
-class Me extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            message: "",
-        };
-    }
 
-    componentDidMount() {
-        let that = this;
+const Me = () => {
+    const [message, setMessage] = useState('');
+    const [title, setTitle] = useState('');
 
-        that.setState({
-            message:
-                <main>
-                    <article>
-
-                        <h2>Min me-sida i kursen jsramverk</h2>
-
-                        <p><img className="me" src={profileImg} width="500" alt="Bild på mig" /></p>
-
-                        <p>Mitt namn är Elena Perers. Född och uppvuxen i Barnaul, en stad som ligger Sydvästra Sibirien i Ryssland. Där växte jag upp, gick i skolan och universitetet.</p>
-
-                        <p>Jag läste sociologi och juridik på Altai State University i Barnaul, Ryssland.</p>
-
-                        <p>Mitt första möte med programmering fick jag i högstadiet då vi fick lära oss basic. Det kändes för komplicerat för mig på den tiden och bestämde mig för att inte bli utvecklare. Men nu sitter jag här och studerar webbprogramering. Intresset väcktes för inte så länge sedan när jag flyttade till Sverige bestämde jag mig för att byta yrke helt. Jag började bli nyfiken på hur fungerar webbsidor och började titta på video kurser om HTML, CSS och JS och blev förvånad att det är inte så komplicerat och det är inte omöjligt att lära sig.</p>
-                        <p>Vill du läsa mer om vad jag jobbat med så kan du kika på min <a href="https://www.linkedin.com/in/elena-perers/">Linkedin-profil</a>.</p>
-
-                        <p>/Elena Perers</p>
-
-                    </article>
-                </main>
+    useEffect(() => {
+    fetch('https://me-api.elenaperers.me/')
+        .then(res => res.json())
+        .then(function(res) {
+            setTitle(res.data.title);
+            setMessage(res.data.msg);
         });
+    });
 
-    }
-
-    render() {
-        return (
-            this.state.message
-        );
-    }
-}
+    return (
+    <main>
+        <h2>{title}</h2>
+        <article className="me-article">
+            <ReactMarkdown source={ message } />
+        </article>
+    </main>
+    );
+};
 
 export default Me;
